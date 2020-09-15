127decfca1a76c0415204caa4136bf429833a1bd
josé rafael vieira <16804617+jrvieira@users.noreply.github.com>
2020-09-15 04:20:33 +0100

    Update Zero.hs

--- src/Zero.hs
+++ src/Zero.hs
@@ -1,4 +1,11 @@
 module Zero where
 
 count :: Eq a => a -> [a] -> Int
-count x = length . filter (== x)
+count x = go 0
+   where
+   go n [] = n
+   go n (a:as)
+      | x == a = go (succ n) as
+      | otherwise = go n as
+
+--count x = length . filter (== x)
